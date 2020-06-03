5dbbac7bf694a2764f5684c05c24615221a8af2a
JV-Amorim <joaovitoramorimx@gmail.com>
2020-06-03 11:59:04 -0300

    Product and CartItem interfaces have been created. Product mockup added.

---
+++ src/app/models/cart-item.ts
@@ -0,0 +1,7 @@
+import { Product } from './product';
+
+export interface CartItem {
+    product: Product;
+    quantity: number;
+    totalPrice: number;
+}
---
+++ src/app/models/product-mock.ts
@@ -0,0 +1,12 @@
+import { Product } from './product';
+
+export const PRODUCTS: Product[] = [
+    { id: 1, name: "Donut (unidade)", price: 2.99, imagePath: "/products-img/donut.jpg" },
+    { id: 2, name: "Cola-Cola (350ml)", price: 3.49, imagePath: "/products-img/coca-cola.jpg" },
+    { id: 3, name: "Sucrilhos (1kg)", price: 9.99, imagePath: "/products-img/sucrilhos.jpg" },
+    { id: 4, name: "Vinho (1L)", price: 19.99, imagePath: "/products-img/vine.jpg" },
+    { id: 5, name: "Maçã (kg)", price: 3.99, imagePath: "/products-img/apple.png" },
+    { id: 6, name: "Melancia (kg)", price: 3.99, imagePath: "/products-img/watermelon.jpg" },
+    { id: 7, name: "Presunto (kg)", price: 10.99, imagePath: "/products-img/ham.jpg" },
+    { id: 8, name: "Queijo (kg)", price: 19.99, imagePath: "/products-img/cheese.jpg" }
+];
