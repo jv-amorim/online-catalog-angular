ac7da408116719e30f629d5a83bb4fe9f81e66c5
JV-Amorim <joaovitoramorimx@gmail.com>
2020-06-03 18:51:06 -0300

    Adjustments were made to deploy to GitHub Pages.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { CatalogComponent } from './catalog/catalog.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CatalogComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
